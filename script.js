df11b2647ab294017361276a8ee52f720ce88be8
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-01-27 23:58:23 +0600

    Added some js!

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+// lets add some functionality next
+console.log("Calculator script loaded");
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}   
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if (b === 0) {
+        console.error("Cannot divide by zero");
+        return null;
+    }
+    return a / b;
+}
+
+
+
+
+
+
+
+
